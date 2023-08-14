95fba477ed4f9f6d75992fd34d854a21655138aa
Iago Passaglia <iago210669@gmail.com>
2023-08-14 17:00:21 -0300

    Criando tela de mural com todos os pensamentos

---
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-pensamento',
+  templateUrl: './listar-pensamento.component.html',
+  styleUrls: ['./listar-pensamento.component.css']
+})
+export class ListarPensamentoComponent implements OnInit {
+
+  listaPensamentos = [
+    {
+      conteudo: 'juao',
+      autoria: 'Iago',
+      modelo: 'modelo3'
+    },
+
+    {
+      conteudo: 'sad',
+      autoria: 'Iago',
+      modelo: 'modelo1'
+    },
+    {
+      conteudo: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+      autoria: '',
+      modelo: 'modelo1'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
